193a461236972f4203a73ff15d8ee7263da71c74
riserishitha <rishitha.n@kalvium.community>
2025-06-01 21:39:37 +0530

    changes

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -5,7 +5,6 @@ import Certificates from "./Components/Certificates";
 const Home = () => (
   <div id="home">
     <h1>Welcome to My Portfolio</h1>
-    {/* Other sections like About, Projects, Contact can go here */}
   </div>
 );
 

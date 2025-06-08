97cc55bdf105382f44f07bd722021248eda47c58
riserishitha <rishitha.n@kalvium.community>
2025-06-08 20:26:48 +0530

    changes

--- resume/src/Components/Navbar.jsx
+++ resume/src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+            // xmlns="http://www.w3.org/2000/svg"
           >
             {isOpen ? (
               <path

6c158850fdcfce398a382b7a518235439f98846d
riserishitha <rishitha.n@kalvium.community>
2025-06-19 22:32:20 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         Hi, I'm Rishitha Nallapureddy from Kadapa, Andhra Pradesh. I'm currently pursuing my B.Tech in Computer Science Engineering at KL University. I have a strong passion for web development and I'm learning the MERN stack. I also participated in the Smart India Hackathon with my team ‘TechBoo’, where we reached Level 4 and won 1st place at Level 2!
       </p>
       <p>
-        Apart from coding, I enjoy indoor activities and creative projects. I'm adaptable, flexible, and love working on interesting ideas. I’ve also built projects like an e-commerce website for spices and a game called “Dora Bujji”.
+        Apart from coding, I enjoy indoor activities and creative projects. I'm adaptable, flexible, and love working on interesting ideas. I’ve also built projects like an e-commerce website for spices and a game called “Dora Bujji”. I also have experience with software testing.
       </p>
     </div>
   );

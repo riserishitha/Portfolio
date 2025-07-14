a8f580745917a2bf6af892636193379c90a22638
riserishitha <rishitha.n@kalvium.community>
2025-07-14 16:38:21 +0530

    changes

--- resume/src/Components/Contact.jsx
+++ resume/src/Components/Contact.jsx
@@ -3,8 +3,6 @@ const Contact = ()=> {
     <div>
     <h1>This is to contact me.</h1>
     <h3>Through email id : rishitha1826@gmail.com</h3>
-    <br /> 
-    <h3>Phone no : 93918XXX44</h3>
     </div>
     )
     }

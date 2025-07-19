35be7036e627b176e799da3cf7ee4fc8bd750a3b
riserishitha <rishitha.n@kalvium.community>
2025-07-19 16:28:33 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -1,9 +1,7 @@
 const About = ()=> {
     return (
     <div>
-    <h1>This is Rishitha Nallapureddy. I am born and raised in kadapa, Andhra Pradesh.
-         I did my schooling in Nagarjuna model school and passed out with 98 % in my 10th board exam. 
-         I done my 11th and 12th standard in narayan junior college and passed out with 86%</h1>
+    <h1>This is Rishitha Nallapureddy. I am born and raised in kadapa, Andhra Pradesh.</h1>
     </div>
     )
     }

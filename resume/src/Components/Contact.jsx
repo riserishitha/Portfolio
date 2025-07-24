9237fbbd1e8cf920772d60b5abaa94d6623bb8a3
riserishitha <rishitha.n@kalvium.community>
2025-07-24 17:10:46 +0530

    changes

--- resume/src/Components/Contact.jsx
+++ resume/src/Components/Contact.jsx
@@ -1,10 +1,15 @@
-const Contact = ()=> {
+const Contact = () => {
     return (
-    <div>
-    <h1>This is to contact me.</h1>
-    <h2>Mobile No: 9391XXXX44</h2>
-    <h3>Through email id : rishitha1826@gmail.com</h3>
-    </div>
-    )
-    }
-export default Contact
+        <div>
+            <h1>This is to contact me.</h1>
+            <h2>Mobile No: 9391XXXX44</h2>
+            <h3>Email: rishitha1826@gmail.com</h3>
+            <h3>Instagram: <a href="https://instagram.com/rishi_1826" target="_blank" rel="noopener noreferrer">@your_insta_id</a></h3>
+            <h3>LinkedIn: <a href="https://www.linkedin.com/in/rishitha-nallapureddy-4230072a8/" target="_blank" rel="noopener noreferrer">View Profile</a></h3>
+            <h3>GitHub: <a href="https://github.com/riserishitha?tab=overview&from=2025-07-01&to=2025-07-24" target="_blank" rel="noopener noreferrer">github.com/your-github-username</a></h3>
+            <h3>LeetCode: <a href="https://leetcode.com/u/Fzc6umf3yu/" target="_blank" rel="noopener noreferrer">leetcode.com/your_leetcode_id</a></h3>
+        </div>
+    );
+}
+
+export default Contact;

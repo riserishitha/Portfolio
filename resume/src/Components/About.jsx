8281b0b1039df86906dd8e6c0fcb5a6d03a44de8
riserishitha <rishitha.n@kalvium.community>
2025-07-25 16:40:15 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -6,7 +6,6 @@ const About = () => {
       <p className="about-text">I am a Computer Science Engineering student at KL University, passionate about web development and technology.</p>
       <p className="about-text">I have a background in MPC (Maths, Physics, Chemistry) and I'm currently exploring the MERN stack.</p>
       <p className="about-text">I love creating intuitive and responsive user interfaces, and I recently built a spice e-commerce website using the MERN stack.</p>
-      <p className="about-text">I also participated in the national-level Smart India Hackathon (SIH) as part of Team TechBoo and secured 1st place in Level 2.</p>
     </div>
   );
 };

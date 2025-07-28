ee5508d9b3b646662d748661c94daa9bb20b1763
riserishitha <rishitha.n@kalvium.community>
2025-07-28 18:38:17 +0530

    change

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
   return (
     <div className="about-container">
       <h1 className="about-heading">This is Rishitha Nallapureddy.</h1>
-      <p className="about-text">I am a Computer Science Engineering student.</p>
+      <p className="about-text">I am a Computer Science and Engineering student at KLU(kalasalingam institute of technology) in Tamil nadu.</p>
     </div>
   );
 };

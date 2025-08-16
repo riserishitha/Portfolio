ffc61a1a53e90c0eb0ad4c2aac9e8d1884be78e2
riserishitha <rishitha.n@kalvium.community>
2025-08-16 21:04:50 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -3,7 +3,6 @@ const About = () => {
   return (
     <div className="about-container">
       <h1 className="about-heading">This is Rishitha Nallapureddy. I am currently working in ACT Fibernet as a Data Engineer in analystics team.</h1>
-      <p className="about-text">I am a Computer Science and Engineering student at KLU(kalasalingam institute of technology) in Tamil nadu.</p>
     </div>
   );
 };

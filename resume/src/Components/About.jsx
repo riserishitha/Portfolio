376238f2fffa9105827de9fd19358aa658e51cca
riserishitha <rishitha.n@kalvium.community>
2025-08-20 17:41:55 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
   return (
     <div className="about-container">
-      <h1 className="about-heading">This is Rishitha Nallapureddy. I started my schooling in Najarjuna modle school and got out of it with a marks of 590 outpf 600.</h1>
+      <h1 className="about-heading">Rishitha Nallapureddy</h1>
     </div>
   );
 };

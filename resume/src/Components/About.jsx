3abb205f66dbb527f8c471be8255a59298368d7f
riserishitha <rishitha.n@kalvium.community>
2025-08-28 16:01:53 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
   return (
     <div className="about-container">
-      <h1 className="about-heading">Rishitha Nallapureddy</h1>
+      <h1 className="about-heading">This is Rishitha Nallapureddy.</h1>
     </div>
   );
 };

86ff9d28977a4ec0d6ef66b072f3880290589aaa
riserishitha <rishitha.n@kalvium.community>
2025-09-10 09:39:51 +0530

    changes

--- resume/src/Components/About.jsx
+++ resume/src/Components/About.jsx
@@ -2,7 +2,8 @@
 const About = () => {
   return (
     <div className="about-container">
-      <h1 className="about-heading">This is Rishitha Nallapureddy.</h1>
+      <h1 className="about-heading">This is Rishitha Nallapureddy. I am currently wotking as a Data Enginner in ACT fibernet.
+        Now i am mostly wotking on migration project where i will send data from one db to another db.</h1>
     </div>
   );
 };
